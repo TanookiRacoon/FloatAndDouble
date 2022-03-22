258b14b2408edad3f4ce4e6bc9de8b9e604a9927
Amelia <ames.langford@gmail.com>
2022-03-22 17:25:23 -0400

    Updated Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// writde here
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum Value = " + myMinFloatValue);
+        System.out.println("Float Maximum Value = " + myMaxFloatValue);
     }
 }
